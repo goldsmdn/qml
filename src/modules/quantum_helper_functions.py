194b5e4be5794f604a8f8077fb576335cb0b6f73
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2025-11-17 14:07:31 +0000

    Document function module

--- src/modules/quantum_helper_functions.py
+++ src/modules/quantum_helper_functions.py
@@ -32,15 +32,11 @@ def find_theta(s:int, j:int, features:list[float]) -> float:
     numerator, denominator = 0.0, 0.0
 
     for l in range(1, 2**(s-1) + 1):
-        #print(f'{l=}')
         index = (2*j - 1) * 2**(s-1) + l - 1
-        #print(f'{index=}')
         numerator += features[index]**2
 
     for l in range(1, 2**s + 1):
-        #print(f'{l=}')
         index = (2*j - 2) * 2**(s-1) + l - 1
-        #print(f'{index=}')
         denominator += features[index]**2
     if denominator == 0:
         theta = 0.0
@@ -50,7 +46,6 @@ def find_theta(s:int, j:int, features:list[float]) -> float:
 
 def convert_int_to_bin_list(value:int, length:int) -> list[int]:
     """Convert integer to binary list of given length"""
-    #print(f'Converting value={value} to binary list')
     format_string = '0' + str(length) + 'b'
     bin_list = list(map(int, format(value, format_string)))
     return bin_list
@@ -63,29 +58,24 @@ def set_Paulix_controls(bin_list:list[int], wires:list[str]) -> None:
 
 def my_amplitude_encoding(features: list[float], wires: list[str]) -> None:
     """Custom amplitude encoding function"""
-    #norm = find_norm(features)
-    #if abs(norm - 1.0) > ABS:
-    #    raise Exception(f'Feature vector not normalised, norm={norm:.2f}')
     validate_feature_list(features, wires)
     import pennylane as qml
-    #for s in range(1, len(wires)+1):
     for s in range(len(wires), 0, -1):
+        #s counts down from n_qubits to 1
         qubit = len(wires) - s + 1
-        #print(f'Encoding on qubit {qubit} (wire {wires[qubit-1]}) with s={s}')
+        #qubit counts up from 1
         active_wire = wires[qubit-1]
         for j in range(1, 2**(qubit-1) + 1):
-            #print(f's={s}, j={j}')
             theta = find_theta(s, j, features)
-            #print(f'theta={theta:.3f}')
             if qubit == 1:
                 # top wire, just apply RY
                 qml.RY(theta, wires=[active_wire])
             elif qubit > 1:
-                #print(f's={s}, j={j}')
+                # produce a binary list and use it to set controls for the relevant wires
                 bin_list = convert_int_to_bin_list(j-1, len(wires)- s)
-                #print(f'bin_list={bin_list}')
                 set_Paulix_controls(bin_list, wires)
                 control_wires = wires[:qubit-1]
-                #print(f'creating controlled RY on wire {active_wire} with controls {control_wires}')
+                # controlled RY rotation on active wire
                 qml.ctrl(qml.RY, control=control_wires)(theta, wires=[active_wire])
+                #undo the controls by repeating
                 set_Paulix_controls(bin_list, wires)                   
